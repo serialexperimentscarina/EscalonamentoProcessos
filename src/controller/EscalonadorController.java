b5bacfcd0a7cb5a30944f15190f7e398c7528580
serialexperimentscarina <108838850+serialexperimentscarina@users.noreply.github.com>
2023-04-21 23:00:12 -0300

    refactor: comments

--- src/controller/EscalonadorController.java
+++ src/controller/EscalonadorController.java
@@ -5,6 +5,9 @@ import model.Processo;
 
 public class EscalonadorController {
 	
+	// Recebe uma Fila de processos, como parâmetros, verifica se o número
+	// que vezes é maior que 1, decrementa esse valor no objeto e, se for maior que 1, insere o objeto
+	// novamente na fila;
 	public void escalonador(FilaObject fila) throws Exception {
 		Processo processo = (Processo) fila.remove();
 		if (processo.qtdRetornos > 1) {
