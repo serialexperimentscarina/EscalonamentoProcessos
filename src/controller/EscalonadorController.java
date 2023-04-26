65d4abb08f2c3c398059d849c7f3ff61f38ce3e0
serialexperimentscarina <108838850+serialexperimentscarina@users.noreply.github.com>
2023-04-25 23:38:18 -0300

    fix: process' return value

--- src/controller/EscalonadorController.java
+++ src/controller/EscalonadorController.java
@@ -6,12 +6,12 @@ import model.Processo;
 public class EscalonadorController {
 	
 	// Recebe uma Fila de processos, como parâmetros, verifica se o número
-	// que vezes é maior que 1, decrementa esse valor no objeto e, se for maior que 1, insere o objeto
+	// de vezes é maior que 1, decrementa esse valor no objeto e, se for maior que 1, insere o objeto
 	// novamente na fila;
 	public void escalonador(FilaObject fila) throws Exception {
 		Processo processo = (Processo) fila.remove();
-		if (processo.qtdRetornos > 1) {
-			processo.qtdRetornos--;
+		processo.qtdRetornos--;
+		if (processo.qtdRetornos >= 1) {
 			fila.insert(processo);
 		}
 		System.out.println("O processo " + processo + " foi executado.");
